4a0cb9d9af268ec4621a13da2539640f891ff217
Alexander Jocobi <alexander.vazquez@g-global.com>
2022-04-10 00:12:24 -0700

    add keys for insegure deploy

---
+++ src/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    database: {
+        host: 'bujpzyd5isl9ekctipbu-mysql.services.clever-cloud.com',
+        user: 'ug6dnnvtnruhm0nh',
+        password: 'BMjeJrUkmiT7iQVanilP',
+        database: 'bujpzyd5isl9ekctipbu'
+    }
+    //database: {
+    //    host: 'localhost',
+    //    user: 'root',
+    //    password: '',
+    //    database: 'runnerx'
+    //}
+};
