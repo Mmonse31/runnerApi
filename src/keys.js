a5b3368d1bc9453eaf2551504249b859dea2a755
Montserrat Montaño <0318119070@ut-tijuana.edu.mx>
2022-04-10 20:08:29 -0700

    update keys

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        host: 'bujpzyd5isl9ekctipbu-mysql.services.clever-cloud.com',
-        user: 'ug6dnnvtnruhm0nh',
-        password: 'BMjeJrUkmiT7iQVanilP',
-        database: 'bujpzyd5isl9ekctipbu'
+        host: 'bd5ukvgnazzmn9rhb6ed-mysql.services.clever-cloud.com',
+        user: 'ujdmvzbkhqa5ugnt',
+        password: 'F9i5uIqRRjLHgkznLQpe',
+        database: 'bd5ukvgnazzmn9rhb6ed'
     }
     //database: {
     //    host: 'localhost',
